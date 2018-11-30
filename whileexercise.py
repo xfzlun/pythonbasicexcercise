6f7b4dd32b18c0086c4458b505e9102b54509a73
xfzlun <xfz79@foxmail.com>
2018-11-30 21:56:50 +0800

    update whileexercise

---
+++ whileexercise.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+'''
+@Time:2018-11-30;21:38
+@author: aLuren
+@FileName: whileexercise.py
+@SoftWare:PyCharm
+COPYRIGHT(C)2018 SZ-MB-QTC
+'''
+a = [11, 22, 33, 44 ]
+i = 0
+while i < len(a):
+    print('第',i, '个元素为：', a[i])
+    i += 1
+

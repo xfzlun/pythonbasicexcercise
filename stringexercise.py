7cb5f6fe823a299ff5aef8cd6c9ce65ecbb22722
xfzlun <1020976137@qq.com>
2018-12-02 14:14:34 +0800

    string exercise

---
+++ stringexercise.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+'''
+#@Time  :2018-12-02;14:03
+@author: aLuren
+#@FileName: stringexercise.py
+#@Software: PyCharm
+Copyright(C)2018 SZ-MB-QTC
+字符串可以执行的操作：
+1. 合并
+2. 重复
+3. 索引
+4. 切片
+5. 成员资格in/not in
+6. 长度
+7. 最大值
+8. 最小值
+'''
+
+a = 'apple'
+b = 'orange'
+c = 'apple1234'
+print(a+b)
+print(a * 2)
+print(a[2])
+print(b[-3])
+

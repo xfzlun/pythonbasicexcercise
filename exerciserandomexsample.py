b1b9ccfb8a391679957d15e92fe2cf5174c64d3c
xfzlun <xfz79@foxmail.com>
2018-12-03 22:37:53 +0800

    Difference between "import random" and "from random import *"

--- exerciserandomexsample.py
+++ exerciserandomexsample.py
@@ -8,11 +8,10 @@
 Copyright(C)2018 SZ-MB-QTC
 主旨：random函数的使用
 '''
-
 import random
-x = random()
-a = seed(x)  #seed的用法？？
-h = randrange(0, 100, 3)
+x = random.random()
+a = random.seed(x)  #seed的用法？？
+h = random.randrange(0, 100, 3)
 print(a)
 print(x)
 print(h)
@@ -21,6 +20,7 @@ print(h)
 seed
 shuffle
 这两个函数还要查一下用法，比较模糊
+如果要使用import random, 则要加上模块名的限定，但是如果使用from random import *, 则不用加上模块名的限定
 '''
 
 

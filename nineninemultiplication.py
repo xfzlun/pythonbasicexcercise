37d6203bec2b62c3c36587750d76ccb64c44c8a9
xfzlun <xfz79@foxmail.com>
2018-12-04 23:20:22 +0800

    nine nine multiplication

--- nineninemultiplication.py
+++ nineninemultiplication.py
@@ -8,3 +8,8 @@ COPYRIGHT(C)2018 SZ-MB-QTC
 题目：循环的嵌套，案例99乘法表
 '''
 
+for row in range(1,10):
+    for col in range(row,10):
+        #print(row,'x',col,'=',row*col)
+        print(row, 'x', col, '=', row * col,'  ',end='')  #end是什么意思？
+        print('/n')

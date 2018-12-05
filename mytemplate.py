a9508a2dc4f04454c32366d41fbe9e5d3ef6c7d4
xfzlun <1020976137@qq.com>
2018-12-05 16:52:57 +0800

    add python template

---
+++ mytemplate.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+'''
+#@Time  :2018-12-05;16:52
+@author: aLuren
+
+#@FileName: mytemplate.py
+#@Software: PyCharm
+Copyright(C)2018 SZ-MB-QTC
+'''
+
+'''
+# _*_ coding: utf-8 _*_
+'''
+#@Time  :${YEAR}-${MONTH}-${DAY};${TIME}
+@author: aLuren
+
+#@FileName: ${NAME}.py
+#@Software: ${PRODUCT_NAME}
+Copyright(C)${YEAR} SZ-MB-QTC
+'''
+'''
